5be0d314079af9f71a5043a02e73dfdd1da1a869
SirMorthwyl <rodrigosantosmorgado@gmail.com>
2021-09-17 18:55:12 +0100

    Auto assign role on join

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 const { Client, Intents, Collection } = require("discord.js");
-const { token } = require("./config.json");
+const { token, visitorRoleId } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MEMBERS,
+    ],
+});
 
 client.commands = new Collection();
 const commandFiles = fs
@@ -35,6 +41,22 @@ client.on("interactionCreate", async (interaction) => {
             content: "There was an error while executing this command!",
             ephemeral: true,
         });
+    }
+});
+
+client.on("guildMemberAdd", async (member) => {
+    try {
+        if (member.user.bot) return;
+
+        let role = await member.guild.roles.fetch(visitorRoleId);
+        if (!role) {
+            console.error(`Couldn't find role with id ${visitorRoleId}.`);
+            return;
+        }
+
+        await member.roles.add(role);
+    } catch (error) {
+        console.error(error);
     }
 });
 
